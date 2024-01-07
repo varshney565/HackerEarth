059ddc02ea7facbb2f2e8658a9edfc574d610d45
shivam <uietcse1923shivamvarshney@kuk.ac.in>
2024-01-07 16:28:03 +0530

    closer

---
+++ algorithemRandom/closer.cpp
@@ -0,0 +1,103 @@
+/**
+ * author : shivam varshney
+ * UIET KURUKSHETRA UNIVERSITY
+ * ∑.⌣
+ * */
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+#define pbds tree<int, null_type,less_equal<int>, rb_tree_tag,tree_order_statistics_node_update>
+#include<bits/stdc++.h>
+#pragma GCC optimise("Ofast")
+#pragma GCC target("avx","avx2","fma")
+#pragma GCC optimization ("O3")
+#pragma GCC optimization ("unroll-loops")
+using namespace std;
+#define int long long
+#define all(x) x.begin(),x.end()
+#define pi pair<int,int>
+#define ff first
+#define ss second
+
+#ifndef ONLINE_JUDGE
+#define debug(x) cerr << #x <<" "; _print(x); cerr << "\n";
+#else
+#define debug(x)
+#endif
+
+void _print(int t) {cerr << t;}
+void _print(string t) {cerr << t;}
+void _print(char t) {cerr << t;}
+void _print(double t) {cerr << t;}
+void _print(bool t) {cerr << t;}
+
+template <class T, class V> void _print(pair <T, V> p);
+template <class T> void _print(vector <T> v);
+template <class T> void _print(set <T> v);
+template <class T, class V> void _print(map <T, V> v);
+template <class T> void _print(multiset <T> v);
+template <class T, class V> void _print(pair <T, V> p) {cerr << "{"; _print(p.ff); cerr << ","; _print(p.ss); cerr << "}";}
+template <class T> void _print(vector <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(set <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(multiset <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T, class V> void _print(map <T, V> v) {cerr << "[ "; for (auto i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T, class V> void _print(multimap <T, V> v) {cerr << "[ "; for (auto i : v) {_print(i); cerr << " ";} cerr << "]";}
+inline void read(int &n,vector<int> &v) {for(int i = 0 ; i < n ; i++) cin>>v[i];}
+inline void print(int &n,vector<int> &v) {for(int i = 0 ; i < n ; i++) cout<<v[i]<<" ";cout<<"\n";}
+inline void read(int &n,int &m,vector<vector<int>> &v) {for(int i = 0 ; i < n ; i++) {for(int j = 0 ; j < m ; j++){ cin>>v[i][j];}}}
+inline void print(int &n,int &m,vector<vector<int>> &v) {for(int i = 0 ;i < n ; i++){for(int j = 0 ; j < m ; j++){cout<<v[i][j]<<" ";}cout<<"\n";}}
+inline void consoleLog(bool ans) {cout<<(ans ? "YES\n" : "NO\n");}
+inline int get(pbds &s,int ind) {return *s.find_by_order(ind);}
+inline int lb(pbds &s,int val) {return s.order_of_key(val);}
+
+
+void solve() {
+	int n,x;
+	cin>>n>>x;
+	vector<int> v(n);
+	read(n,v);
+	int ans = 0;
+	sort(all(v));
+    int low = v[0],high = v[n-1];
+    vector<int> f(high+1,0);
+    for(int i = 0 ; i < n ; i++) {
+        f[v[i]]++;
+    }
+    while(high - low > x) {
+        if(f[low] < f[high]) {
+            ans += f[low];
+            f[high] -= f[low];
+            f[high-1] += f[low];
+            f[low+1] += f[low];
+            f[low] = 0;
+            low++;
+        }else {
+            ans += f[high];
+            f[low] -= f[high];
+            f[low+1] += f[high];
+            f[high-1] += f[high];
+            f[high] = 0;
+            high--;
+        }
+    }
+	cout<<ans<<"\n";
+}
+
+signed main()
+{
+    #ifndef ONLINE_JUDGE
+        freopen("./../io/input.txt","r",stdin);
+        freopen("./../io/output.txt","w",stdout);
+        freopen("./../io/error.txt", "w", stderr);
+    #endif
+
+    ios_base ::sync_with_stdio(false);
+    cin.tie(NULL);
+    int t = 1;
+    // cin>>t;
+    while(t--) {
+        solve();
+    }
+    cerr<<"time taken : "<<(float)clock()/CLOCKS_PER_SEC<<" secs"<<"\n";
+    return 0;
+} 
