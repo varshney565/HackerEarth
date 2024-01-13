17b3934f90198145a267b26af253bfa41ab28c74
shivam <uietcse1923shivamvarshney@kuk.ac.in>
2024-01-13 15:46:35 +0530

    gcd_strings

---
+++ basicProgrammingRecursion/gcd_strings.cpp
@@ -0,0 +1,104 @@
+/**
+ * author : shivam varshney
+ * UIET KURUKSHETRA UNIVERSITY
+ * ∑.⌣
+ * */
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+#define pbds tree<int, null_type,less_equal<int>, rb_tree_tag,tree_order_statistics_node_update>
+#include<bits/stdc++.h>
+#pragma GCC optimise("Ofast")
+#pragma GCC target("avx","avx2","fma")
+#pragma GCC optimization ("O3")
+#pragma GCC optimization ("unroll-loops")
+using namespace std;
+#define all(x) x.begin(),x.end()
+#define pi pair<int,int>
+#define ff first
+#define ss second
+ 
+#ifndef ONLINE_JUDGE
+#define debug(x) cerr << #x <<" "; _print(x); cerr << "\n";
+#else
+#define debug(x)
+#endif
+ 
+void _print(int t) {cerr << t;}
+void _print(string t) {cerr << t;}
+void _print(char t) {cerr << t;}
+void _print(double t) {cerr << t;}
+void _print(bool t) {cerr << t;}
+ 
+template <class T, class V> void _print(pair <T, V> p);
+template <class T> void _print(vector <T> v);
+template <class T> void _print(set <T> v);
+template <class T, class V> void _print(map <T, V> v);
+template <class T> void _print(multiset <T> v);
+template <class T, class V> void _print(pair <T, V> p) {cerr << "{"; _print(p.ff); cerr << ","; _print(p.ss); cerr << "}";}
+template <class T> void _print(vector <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(set <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(multiset <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T, class V> void _print(map <T, V> v) {cerr << "[ "; for (auto i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T, class V> void _print(multimap <T, V> v) {cerr << "[ "; for (auto i : v) {_print(i); cerr << " ";} cerr << "]";}
+inline void read(int &n,vector<int> &v) {for(int i = 0 ; i < n ; i++) cin>>v[i];}
+inline void print(int &n,vector<int> &v) {for(int i = 0 ; i < n ; i++) cout<<v[i]<<" ";cout<<"\n";}
+inline void read(int &n,int &m,vector<vector<int>> &v) {for(int i = 0 ; i < n ; i++) {for(int j = 0 ; j < m ; j++){ cin>>v[i][j];}}}
+inline void print(int &n,int &m,vector<vector<int>> &v) {for(int i = 0 ;i < n ; i++){for(int j = 0 ; j < m ; j++){cout<<v[i][j]<<" ";}cout<<"\n";}}
+inline void consoleLog(bool ans) {cout<<(ans ? "YES\n" : "NO\n");}
+inline int get(pbds &s,int ind) {return *s.find_by_order(ind);}
+inline int lb(pbds &s,int val) {return s.order_of_key(val);}
+ 
+ 
+const int mod = 1e9+7;
+ 
+int power(int a,int b) {
+    if(b == 0) {
+        return 1;
+    } 
+    int smallAns = power(a,b/2);
+    int ans = ((long long)smallAns*smallAns)%mod;
+    if(b%2) {
+        ans = ((long long)ans*a)%mod;
+    }
+    return ans;
+}
+ 
+pair<int,int> go(int a,int b) {
+    if(a%b == 0) {
+        return {power(2,a-1),power(2,b-1)};
+    }
+    auto res = go(b,a%b);
+    int x = res.first;
+    int y = res.second;
+    int n = a/b;
+    int r = power(2,b);
+    int ans = ((((long long)x*((power(r,n)-1+mod)%mod))%mod)*power((r-1+mod)%mod,mod-2))%mod;
+    ans = (((long long)ans*power(2,a%b))%mod+y)%mod;
+    return {ans,x};
+};
+
+void solve() {
+    int n,x;
+    cin>>n>>x;
+    cout<<go(n,x).first<<"\n";
+}
+ 
+signed main()
+{
+    #ifndef ONLINE_JUDGE
+        freopen("./../io/input.txt","r",stdin);
+        freopen("./../io/output.txt","w",stdout);
+        freopen("./../io/error.txt", "w", stderr);
+    #endif
+ 
+    ios_base ::sync_with_stdio(false);
+    cin.tie(NULL);
+    int t = 1;
+    cin>>t;
+    while(t--) {
+        solve();
+    }
+    cerr<<"time taken : "<<(float)clock()/CLOCKS_PER_SEC<<" secs"<<"\n";
+    return 0;
+} 
