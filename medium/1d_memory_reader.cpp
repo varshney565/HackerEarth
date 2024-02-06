c1e5747bb53b8081cbbde58fd3980620a5be094d
shivam <uietcse1923shivamvarshney@kuk.ac.in>
2024-02-06 16:09:01 +0530

    memory reader

---
+++ medium/1d_memory_reader.cpp
@@ -0,0 +1,117 @@
+/**
+ * author : shivam varshney
+ * UIET KURUKSHETRA UNIVERSITY
+ * ∑.⌣
+ * */
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+#define pbds tree<int, null_type,less_equal<int>, rb_tree_tag,tree_order_statistics_node_update>
+#include<bits/stdc++.h>
+#pragma GCC optimise("Ofast")
+#pragma GCC target("avx","avx2","fma")
+#pragma GCC optimization ("O3")
+#pragma GCC optimization ("unroll-loops")
+using namespace std;
+// #define int long long
+#define all(x) x.begin(),x.end()
+#define pi pair<int,int>
+#define ff first
+#define ss second
+
+#ifndef ONLINE_JUDGE
+#define debug(x) cerr << #x <<" "; _print(x); cerr << "\n";
+#else
+#define debug(x)
+#endif
+
+void _print(int t) {cerr << t;}
+void _print(string t) {cerr << t;}
+void _print(char t) {cerr << t;}
+void _print(double t) {cerr << t;}
+void _print(bool t) {cerr << t;}
+
+template <class T, class V> void _print(pair <T, V> p);
+template <class T> void _print(vector <T> v);
+template <class T> void _print(set <T> v);
+template <class T, class V> void _print(map <T, V> v);
+template <class T> void _print(multiset <T> v);
+template <class T, class V> void _print(pair <T, V> p) {cerr << "{"; _print(p.ff); cerr << ","; _print(p.ss); cerr << "}";}
+template <class T> void _print(vector <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(set <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(multiset <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T, class V> void _print(map <T, V> v) {cerr << "[ "; for (auto i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T, class V> void _print(multimap <T, V> v) {cerr << "[ "; for (auto i : v) {_print(i); cerr << " ";} cerr << "]";}
+inline void read(int &n,vector<int> &v) {for(int i = 0 ; i < n ; i++) cin>>v[i];}
+inline void print(int &n,vector<int> &v) {for(int i = 0 ; i < n ; i++) cout<<v[i]<<" ";cout<<"\n";}
+inline void read(int &n,int &m,vector<vector<int>> &v) {for(int i = 0 ; i < n ; i++) {for(int j = 0 ; j < m ; j++){ cin>>v[i][j];}}}
+inline void print(int &n,int &m,vector<vector<int>> &v) {for(int i = 0 ;i < n ; i++){for(int j = 0 ; j < m ; j++){cout<<v[i][j]<<" ";}cout<<"\n";}}
+inline void consoleLog(bool ans) {cout<<(ans ? "YES\n" : "NO\n");}
+inline int get(pbds &s,int ind) {return *s.find_by_order(ind);}
+inline int lb(pbds &s,int val) {return s.order_of_key(val);}
+
+/**
+ * 
+ * */
+
+void solve() {
+	int n;
+	cin>>n;
+	vector<int> v(n);
+	read(n,v);
+    map<int,int> f;
+    for(int i = 0 ; i < n ; i++) {
+        f[v[i]] = i+1;
+    }
+    vector<map<int,bool>> dp(n+1,map<int,bool>());
+    int cnt = 0;
+	function<bool(int,int)> go = [&](int ind,int L) {
+        /////////////////////////////////////////////////////////
+        /// For Debugging 
+
+        /////////////////////////////////////////////////////////
+        if(ind == n) {
+            return true;
+        }
+        dp[ind][L] = false;
+        bool res = false;
+        for(int i = -1 ; i <= 1 ; i++) {
+            int newL   = L+i;
+            int newpos = v[ind-1]+newL;
+            if(f.find(newpos) != f.end()) {
+                int newInd = f[newpos];
+                if(dp[newInd].find(newL) != dp[newInd].end()) {
+                    continue;
+                }
+                if(go(newInd,newL)) {
+                    res = true;
+                    break;
+                };
+            }
+        }
+        return dp[ind][L] = res;
+	};
+
+    bool ans = go(1,v[0]);
+    if(ans) cout<<"YES\n";
+    else cout<<"NO\n";
+}
+
+signed main()
+{
+    #ifndef ONLINE_JUDGE
+        freopen("./../io/input.txt","r",stdin);
+        freopen("./../io/output.txt","w",stdout);
+        freopen("./../io/error.txt", "w", stderr);
+    #endif
+
+    ios_base ::sync_with_stdio(false);
+    cin.tie(NULL);
+    int t;
+    cin>>t;
+    while(t--) {
+        solve();
+    }
+    cerr<<"time taken : "<<(float)clock()/CLOCKS_PER_SEC<<" secs"<<"\n";
+    return 0;
+} 
