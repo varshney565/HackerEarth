3586d8daf4b201ebcd9611af2112520ace4c494f
shivam <uietcse1923shivamvarshney@kuk.ac.in>
2024-02-07 17:10:34 +0530

    non intersection segment LIS(BS)

---
+++ medium/1d_non_intersection_segments.cpp
@@ -0,0 +1,116 @@
+/**
+ * author : shivam varshney
+ * UIET KURUKSHETRA UNIVERSITY
+ * ∑.⌣
+ * */
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+#define pbds tree<int, null_type,less_equal<int>, rb_tree_tag,tree_order_statistics_node_update>
+#include<bits/stdc++.h>
+#pragma GCC optimise("Ofast")
+#pragma GCC target("avx","avx2","fma")
+#pragma GCC optimization ("O3")
+#pragma GCC optimization ("unroll-loops")
+using namespace std;
+#define int long long
+#define all(x) x.begin(),x.end()
+#define pi pair<int,int>
+#define ff first
+#define ss second
+
+#ifndef ONLINE_JUDGE
+#define debug(x) cerr << #x <<" "; _print(x); cerr << "\n";
+#else
+#define debug(x)
+#endif
+
+void _print(int t) {cerr << t;}
+void _print(string t) {cerr << t;}
+void _print(char t) {cerr << t;}
+void _print(double t) {cerr << t;}
+void _print(bool t) {cerr << t;}
+
+template <class T, class V> void _print(pair <T, V> p);
+template <class T> void _print(vector <T> v);
+template <class T> void _print(set <T> v);
+template <class T, class V> void _print(map <T, V> v);
+template <class T> void _print(multiset <T> v);
+template <class T, class V> void _print(pair <T, V> p) {cerr << "{"; _print(p.ff); cerr << ","; _print(p.ss); cerr << "}";}
+template <class T> void _print(vector <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(set <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(multiset <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T, class V> void _print(map <T, V> v) {cerr << "[ "; for (auto i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T, class V> void _print(multimap <T, V> v) {cerr << "[ "; for (auto i : v) {_print(i); cerr << " ";} cerr << "]";}
+inline void read(int &n,vector<int> &v) {for(int i = 0 ; i < n ; i++) cin>>v[i];}
+inline void print(int &n,vector<int> &v) {for(int i = 0 ; i < n ; i++) cout<<v[i]<<" ";cout<<"\n";}
+inline void read(int &n,int &m,vector<vector<int>> &v) {for(int i = 0 ; i < n ; i++) {for(int j = 0 ; j < m ; j++){ cin>>v[i][j];}}}
+inline void print(int &n,int &m,vector<vector<int>> &v) {for(int i = 0 ;i < n ; i++){for(int j = 0 ; j < m ; j++){cout<<v[i][j]<<" ";}cout<<"\n";}}
+inline void consoleLog(bool ans) {cout<<(ans ? "YES\n" : "NO\n");}
+inline int get(pbds &s,int ind) {return *s.find_by_order(ind);}
+inline int lb(pbds &s,int val) {return s.order_of_key(val);}
+
+void solve() {
+	int n;
+	cin>>n;
+	vector<pair<int,int>> v;
+	vector<vector<int>> dem;
+	for(int i = 0 ; i < n ; i++) {
+		int a,b;cin>>a>>b;
+		int c,d;cin>>c>>d;
+		dem.push_back({a,b,c,d});
+	}
+	if(n == 1) {
+		cout<<1<<"\n";
+		return;
+	}
+	if(dem[0][0] == dem[1][0]) {
+		for(int i = 0 ; i < n ; i++) {
+			v.push_back({dem[i][1],dem[i][3]});
+		}
+	}else {
+		for(int i = 0 ; i < n ; i++) {
+			v.push_back({dem[i][0],dem[i][2]});
+		}
+	}
+	debug(v);
+	sort(all(v));
+	// Now find LIS on the second value of each pair
+	vector<int> lis;
+	for(int i = 0 ; i < n ; i++) {
+		int s = 0;
+		int e = lis.size()-1;
+		while(s <= e) {
+			int mid = s + (e - s)/2;
+			if(lis[mid] > v[i].second) {
+				e = mid - 1;
+			}else {
+				s = mid + 1;
+			}
+		}
+		if(s == lis.size()) {
+			lis.push_back(v[i].second);
+		}else {
+			lis[s] = v[i].second;
+		}
+ 	}
+ 	cout<<lis.size()<<"\n";
+}
+
+signed main()
+{
+    #ifndef ONLINE_JUDGE
+        freopen("./../io/input.txt","r",stdin);
+        freopen("./../io/output.txt","w",stdout);
+        freopen("./../io/error.txt", "w", stderr);
+    #endif
+
+    ios_base ::sync_with_stdio(false);
+    cin.tie(NULL);
+    int t = 1;
+    while(t--) {
+        solve();
+    }
+    cerr<<"time taken : "<<(float)clock()/CLOCKS_PER_SEC<<" secs"<<"\n";
+    return 0;
+} 
